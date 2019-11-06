6a62c7dd960ff4255e593113ac3ddc16b9dc0e28
owinolawrence <owinolawrence21@gmail.com>
2019-11-06 16:24:45 +0300

    first commit

---
+++ input_demo.py
@@ -0,0 +1,23 @@
+# print("What is your name?")
+# name= input()
+
+# print("How old are you?")
+# age=float(input())
+# print(name)
+# print(age)
+# import sys
+# name=sys.argv[1]
+# age=sys.argv [2]
+# print("how old are you")
+# age = int(input())
+# print=name
+# print=age
+import datetime
+
+username =input("Enter your name")
+userage  = input("Enter your age")
+now =datetime.datetime.now()
+diff =  80-int(userage)
+print("hello" + username +"" + "You will be 80years in",(now.year + diff))
+# print(username)
+# print(userage)
